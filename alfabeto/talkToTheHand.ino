23b941be2e72fd22cb6d503816ad26486275aeda
BrunaAraujo <32269613+BrunaAraujo@users.noreply.github.com>
2021-08-01 03:27:29 -0300

    Update talkToTheHand.ino

--- alfabeto/talkToTheHand.ino
+++ alfabeto/talkToTheHand.ino
@@ -1,97 +1,51 @@
 // O tempo para leitura pela função analogRead() é por volta de 100 micro segundos -> https://www.embarcados.com.br/arduino-entradas-analogicas/
 
-// leds do display de 7 segmentos
-int LEDs[] = {22,23,1,3,19,18,5,21};
-// pinos analógicos para leitura
-int analogPins[] = {13, 12, 14, 27, 26}; // leds para a leitura dos resistores flex
-// valor do giroscopio -> eixo y (em cima e em baixo)
-int eixoy = 0;
-int eixoy_final = 0;
-int eixoy_port = 25;
-// valor do giroscopio -> eixo z (giro da mão)
-int eixoz = 0;
-int eixoz_final = 0;
-int eixoz_port = 33;
-// valores de cada dedo
-int dedos[] = {0, 0, 0, 0, 0};
-int dedos_final[] = {0, 0, 0, 0, 0};
-// valor do sensor de pressão do dedão e indicador
-int dedao = 0;
-int dedao_final = 0;
-int dedao_port = 25;
-// valor do sensor de pressão do dedão e indicador
-int indicador = 0;
-int indicador_final = 0;
-int indicador_port = 25;
 // referência que define se o dedo está dobrado ou não
-int ref = 500;
+int ref_flex = 500; /* apenas para testes */
+int ref_pressao = 500;
+int ref_eixoy = 500;
+int ref_eixoz = 500;
 
-// ======================================================================================================================================================================================
-// letras do alfabeto -> led states to display
-int nda[] = {0, 0, 0, 0, 0, 0, 0, 1};   // nenhuma letra
-int a[] = {1, 1, 1, 0, 1, 1, 1, 1};   // A
-int b[] = {1, 1, 1, 1, 1, 0, 0, 0};   // b
-int c[] = {0, 1, 1, 1, 0, 0, 1, 0};   // C
-int d[] = {1, 0, 1, 1, 1, 1, 0, 0};   // d
-int e[] = {1, 1, 1, 1, 0, 0, 1, 0};   // E
-int f[] = {1, 1, 1, 0, 0, 0, 1, 0};   // F
-int g[] = {0, 1, 1, 1, 1, 0, 1, 0};   // G
-int h[] = {1, 1, 1, 0, 1, 0, 0, 0};   // h
-int ii[] = {0, 1, 1, 0, 0, 0, 0, 0};   // I
-int j[] = {0, 0, 1, 1, 1, 1, 0, 0};   // J
-int k[] = {1, 1, 1, 0, 1, 1, 0, 1};   // K = X.
-int l[] = {0, 1, 1, 1, 0, 0, 0, 0};   // L
-int m[] = {1, 0, 1, 0, 1, 0, 0, 1};   // m = n.
-int n[] = {1, 0, 1, 0, 1, 0, 0, 0};   // n
-int o[] = {0, 1, 1, 1, 1, 1, 1, 0};   // O
-int p[] = {1, 1, 1, 0, 0, 1, 1, 0};   // P
-int q[] = {0, 1, 1, 1, 1, 1, 1, 1};   // Q = O.
-int r[] = {1, 0, 1, 0, 0, 0, 0, 0};   // r
-int s[] = {1, 1, 0, 1, 1, 0, 1, 0};   // S
-int t[] = {1, 1, 1, 1, 0, 0, 0, 0};   // t
-int u[] = {0, 1, 1, 1, 1, 1, 0, 0};   // U
-int v[] = {0, 0, 1, 1, 1, 0, 0, 0};   // v
-int w[] = {0, 0, 1, 1, 1, 0, 0, 1};   // w = v.
-int x[] = {1, 1, 1, 0, 1, 1, 0, 0};   // X
-int y[] = {1, 1, 0, 1, 1, 1, 0, 0};   // y
-int z[] = {1, 0, 1, 1, 0, 1, 1, 0};   // Z
+// valor para testes porque eu fiquei de saco cheio de testar com potenciômetros, e como a gente já sabe que a leitura funciona e o código já ta com o Moisés
+// sensores flex 0-4 / dedao 5 / indicador 6 / eixoy 7 / eixoz 8
+int sensores[] = {0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int sensores_final[] = {0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int sensores_size = 9;
 
+// amostra que pega dos sensores, valores brutos
+int amostra[] = {0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int amostra_final[] = {0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+// ================================================================================================================ LETRAS LIBRAS ===================================================
 // sinais do alfabeto (começando pelo dedão) -> led states to display
-int sinala[] = {0, 1, 1, 1, 1};   // A
-int sinalb[] = {1, 0, 0, 0, 0};   // b
-int sinalc[] = {1, 1, 1, 1, 1};   // C *
-int sinald[] = {1, 0, 1, 1, 1};   // d
-int sinale[] = {1, 1, 1, 1, 1};   // E *
-int sinalf[] = {0, 1, 0, 0, 0};   // F
-int sinalg[] = {1, 0, 1, 1, 1};   // G
-int sinalh[] = {0, 0, 1, 1, 1};   // h
-int sinali[] = {1, 1, 1, 1, 0};   // I
-int sinalj[] = {1, 1, 1, 1, 0};   // J
-int sinalk[] = {0, 0, 1, 1, 1};   // K = X.
-int sinall[] = {0, 0, 1, 1, 1};   // L
-int sinalm[] = {1, 0, 0, 0, 1};   // m = n.
-int sinaln[] = {1, 0, 0, 1, 1};   // n
-int sinalo[] = {1, 1, 1, 1, 1};   // O
-int sinalp[] = {0, 0, 1, 1, 1};   // P
-int sinalq[] = {1, 0, 1, 1, 1};   // Q = O.
-int sinalr[] = {0, 0, 0, 1, 1};   // r
-int sinals[] = {1, 1, 1, 1, 1};   // S
-int sinalt[] = {0, 1, 0, 0, 0};   // t
-int sinalu[] = {1, 0, 0, 1, 1};   // U
-int sinalv[] = {1, 0, 0, 1, 1};   // v
-int sinalw[] = {1, 0, 0, 0, 1};   // w = v.
-int sinalx[] = {1, 1, 1, 1, 1};   // X
-int sinaly[] = {0, 1, 1, 1, 0};   // y
-int sinalz[] = {1, 0, 1, 1, 1};   // Z
-// ======================================================================================================================================================================================
-
-// setando as entradas dos displays de 7 segmentos
-void setup() {
-  Serial.begin(9600);
-  for (int i=0; i<8; i++) {
-    pinMode(LEDs[i], OUTPUT);
-  }
-}
+int sinala[] = {0,1,1,1,1,0,1,1,0};   // A
+int sinalb[] = {1,0,0,0,0,1,1,1,0};   // b
+int sinalc[] = {1,1,1,1,1,0,1,1,0};   // C *
+int sinald[] = {1,0,1,1,1,0,0,1,0};   // d
+int sinale[] = {1,1,1,1,1,0,0,1,0};   // E *
+int sinalf[] = {0,1,0,0,0,1,0,1,0};   // F
+int sinalg[] = {1,0,1,1,1,1,0,1,0};   // G
+int sinalh[] = {0,0,1,1,1,1,0,1,1};   // h
+int sinali[] = {1,1,1,1,0,1,1,1,0};   // I
+int sinalj[] = {1,1,1,1,0,1,1,2,1};   // J
+int sinalk[] = {0,0,1,1,1,1,0,1,0};   // K = X.
+int sinall[] = {0,0,1,1,1,0,0,1,0};   // L
+int sinalm[] = {1,0,0,0,1,1,1,-1,0};   // m = n.
+int sinaln[] = {1,0,0,1,1,1,1,-1,0};   // n
+int sinalo[] = {1,1,1,1,1,1,1,1,1};   // O
+int sinalp[] = {0,0,1,1,1,1,0,-1,0};   // P
+int sinalq[] = {0,0,1,1,1,0,0,-1,0};   // Q = O.
+int sinalr[] = {1,1,0,1,1,1,1,1,0};   // r
+int sinals[] = {1,1,1,1,1,1,1,1,0};   // S
+int sinalt[] = {0,1,0,0,0,0,0,1,0};   // t
+int sinalu[] = {1,0,0,1,1,1,1,1,0};   // U
+int sinalv[] = {1,0,0,1,1,1,0,1,0};   // v
+int sinalw[] = {1,0,0,0,1,1,0,1,0};   // w = v.
+int sinalx[] = {1,1,1,1,1,1,0,2,1};   // X
+int sinaly[] = {0,1,1,1,0,0,1,2,1};   // y
+int sinalz[] = {1,0,1,1,1,1,0,2,1};   // Z
+int sinalespaco[] = {0,0,0,0,0,0,1,2,1};   // Z
+// ================================================================================================================== FUNÇÔES ===================================================
 
 // print dos dedos
 void printaVetor(int* vetor, int tam) {
@@ -102,202 +56,186 @@ void printaVetor(int* vetor, int tam) {
 }
 
 // compara dois verotes de mesmo tamanho e retorna se são ou não iguais
-bool iguais(int *v1, int *v2, int tam) {
-    int i, cont = 1;
-    for(i=0; i < tam; i++) {
-        if (v1[i] != v2[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// escreve no led
-void escreve(int* pos, int tam){
-  for( int i=0 ; i<tam ; i++ ) {
-    if( pos[i] == 1 ) {
-      digitalWrite(LEDs[i], HIGH);
-    }
-    else {
-      digitalWrite(LEDs[i], LOW);
-    }
+  bool iguais(int *v1, int *v2, int tam) {
+      int i, cont = 1;
+      for(i=0; i < tam; i++) {
+          if (v1[i] != 2 && v2[i] != 2 && v1[i] != v2[i]) { // se nenhum dos valores for 2 e forem diferentes: 01, 10, 1-1, -11
+              return false;
+          }
+      }
+      return true;
   }
+
+// ======================================================================================================================================================================================
+
+// configurações
+void setup() {
+  Serial.begin(9600);
 }
 
-
-// MAIN ========================================================================================================================================================================
+// loop
 void loop() {
-  Serial.print("--------->\n");
-  // lendo cada sensor e atribuindo os valores a cada dedo
-  for (int i=0 ; i<5 ; i++ ){
-    if(analogRead(analogPins[i]) > ref) {
-      dedos[i] = 1;
+  Serial.print("---------------------------------------------------------------------->\n");
+  printaVetor(amostra, sensores_size);
+  // Convertendo os valores analógicos recebidos
+  for (int i=0 ; i<5 ; i++ ){ // sensores flex
+    if(amostra[i] > ref_flex) {
+      sensores[i] = 1;
     }
     else {
-      dedos[i] = 0;
-    }
-    if(analogRead(eixoy_port) > ref) {
-      eixoy = 1;
-    }
-    else {
-      eixoy = -1;
-    }
-    if(analogRead(eixoz_port) > ref) {
-      eixoz = 1;
-    }
-    else {
-      eixoz = 0;
+      sensores[i] = 0;
     }
   }
-  dedao = eixoy;
-  indicador = eixoy;
-  printaVetor(dedos, 5);
-  Serial.println(eixoy);
-  Serial.println(eixoz);
-  // esperando para ver se o dedo continua dobrado e se não é sinal intermediário
-  delay(1000); // ms
-  // pegando dados uma segunda vez
-  for (int i=0 ; i<5 ; i++ ){
-    if(analogRead(analogPins[i]) > ref) {
-      dedos_final[i] = 1;
-    }
-    else {
-      dedos_final[i] = 0;
-    }
-    if(analogRead(eixoy_port) > ref) {
-      eixoy_final = 1;
-    }
-    else {
-      eixoy_final = -1;
-    }
-    if(analogRead(eixoz_port) > ref) {
-      eixoz_final = 1;
-    }
-    else {
-      eixoz_final = 0;
-    }
-  }
-  dedao_final = eixoy_final;
-  indicador_final = eixoy_final;
-  printaVetor(dedos_final, 5);
-  Serial.println(eixoy);
-  Serial.println(eixoz);
-  // verificando se os valores coletados batem, menos o eixo z, esse pega apenas maior valor
-  if(eixoy == eixoy_final and iguais(dedos, dedos_final, 5) ){ // eh um sinal e não um intermediário
-    if(eixoz_final > eixoz) { // pega o maior valor que indica a rotação da mão
-      eixoz = eixoz_final;
-    }
-    if(iguais(dedos, sinala, 5)){
-       Serial.println("a");
-       escreve(a, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(iguais(dedos, sinalb, 5)){
-      Serial.println("b");
-       escreve(b, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if( eixoz == 0 and indicador == 1 and iguais(dedos, sinalc, 5)){
-      Serial.println("c");
-       escreve(c, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == 1 and eixoz == 0 and iguais(dedos, sinald, 5)){
-      Serial.println("d");
-       escreve(d, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoz == 0 and iguais(dedos, sinale, 5)){
-      Serial.println("e");
-       escreve(e, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(dedao == 1 and indicador == 0 and iguais(dedos, sinalf, 5)){
-      Serial.println("f");
-       escreve(f, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == 1 and eixoz == 0 and iguais(dedos, sinalg, 5)){
-      Serial.println("g");
-       escreve(g, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(dedao == 1 and eixoy == 1 and eixoz == 1 and iguais(dedos, sinalh, 5)){
-      Serial.println("h");
-       escreve(h, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == 1 and iguais(dedos, sinali, 5)){
-      Serial.println("i");
-       escreve(ii, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == -1 and iguais(dedos, sinalj, 5)){
-      Serial.println("j");
-       escreve(j, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(dedao == 1 and eixoy == 1 and iguais(dedos, sinalk, 5)){
-      Serial.println("k");
-       escreve(k, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == 1 and iguais(dedos, sinall, 5)){
-      Serial.println("l");
-       escreve(l, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == 1 and iguais(dedos, sinalm, 5)){
-      Serial.println("m");
-       escreve(m, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == -1 and iguais(dedos, sinaln, 5)){
-      Serial.println("n");
-       escreve(n, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(iguais(dedos, sinalo, 5)){
-      Serial.println("o");
-       escreve(o, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == -1 and iguais(dedos, sinalp, 5)){
-      Serial.println("p");
-       escreve(p, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == -1 and iguais(dedos, sinalq, 5)){
-      Serial.println("q");
-       escreve(q, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(iguais(dedos, sinalr, 5)){
-      Serial.println("r");
-       escreve(r, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(iguais(dedos, sinals, 5)){
-      Serial.println("s");
-       escreve(s, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(iguais(dedos, sinalt, 5)){
-      Serial.println("t");
-       escreve(t, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(indicador == 1 and eixoy == 1 and iguais(dedos, sinalu, 5)){
-      Serial.println("u");
-       escreve(u, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(indicador == 0 and eixoy == 1 and iguais(dedos, sinalv, 5)){
-      Serial.println("v");
-       escreve(v, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoy == -1 and iguais(dedos, sinalw, 5)){
-      Serial.println("w");
-       escreve(w, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoz == 1 and iguais(dedos, sinalx, 5)){
-      Serial.println("x");
-       escreve(x, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(iguais(dedos, sinaly, 5)){
-      Serial.println("y");
-       escreve(y, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else if(eixoz == 1 and iguais(dedos, sinalz, 5)){
-      Serial.println("z");
-       escreve(z, 8); // tamanho do vetor do display e não dos dedos
-    }
-    else {
-      Serial.println("nda");
-      escreve(nda, 8); // nenhuma letra, desliga o display
-    }
+  if(amostra[5] > ref_pressao) { // sensores de pressao - dedão 
+    sensores[5] = 1;
   }
   else {
-    escreve(nda, 8); // nenhuma letra, desliga o display
+     sensores[5] = 0;
   }
-  
+  if(amostra[6] > ref_pressao) { // sensores de pressao - indicador
+    sensores[6] = 1;
+  }
+  else {
+     sensores[6] = 0;
+  }
+  if(amostra[7] > ref_eixoy) { // aceleropio - eixoy
+    sensores[7] = 1;
+  }
+  else {
+     sensores[7] = -1;
+  }
+  if(amostra[8] > ref_eixoz) { // aceleropio - eixoz
+    sensores[8] = 1;
+  }
+  else {
+     sensores[8] = 0;
+  }
+  // Convertendo os valores analógicos recebidos - parte 2
+  for (int i=0 ; i<5 ; i++ ){ // sensores flex
+    if(amostra_final[i] > ref_flex) {
+      sensores_final[i] = 1;
+    }
+    else {
+      sensores_final[i] = 0;
+    }
+  }
+  if(amostra_final[5] > ref_pressao) { // sensores de pressao - dedão 
+    sensores_final[5] = 1;
+  }
+  else {
+     sensores_final[5] = 0;
+  }
+  if(amostra_final[6] > ref_pressao) { // sensores de pressao - indicador
+    sensores_final[6] = 1;
+  }
+  else {
+     sensores_final[6] = 0;
+  }
+  if(amostra_final[7] > ref_eixoy) { // aceleropio - eixoy
+    sensores_final[7] = 1;
+  }
+  else {
+     sensores_final[7] = -1;
+  }
+  if(amostra_final[8] > ref_eixoz) { // aceleropio - eixoz
+    sensores_final[8] = 1;
+  }
+  else {
+     sensores_final[8] = 0;
+  }
+  printaVetor(sensores, sensores_size);
+  // comparando os valores dos sensores
+  if(iguais(sensores, sensores_final, sensores_size) ){ /* eh um sinal e não um intermediário */
+    if(sensores_final[8] > sensores[8]) { // pega o maior valor que indica a rotação da mão
+      sensores[8] = sensores_final[8];
+    }
+    if(iguais(sensores, sinala, sensores_size)){
+       Serial.println("a");
+    }
+    else if(iguais(sensores, sinalb, sensores_size)){
+      Serial.println("b");
+    }
+    else if(iguais(sensores, sinalc, sensores_size)){
+      Serial.println("c");
+    }
+    else if(iguais(sensores, sinald, sensores_size)){
+      Serial.println("d");
+    }
+    else if(iguais(sensores, sinale, sensores_size)){
+      Serial.println("e");
+    }
+    else if(iguais(sensores, sinalf, sensores_size)){
+      Serial.println("f");
+    }
+    else if(iguais(sensores, sinalg, sensores_size)){
+      Serial.println("g");
+    }
+    else if(iguais(sensores, sinalh, sensores_size)){
+      Serial.println("h");
+    }
+    else if(iguais(sensores, sinali, sensores_size)){
+      Serial.println("i");
+    }
+    else if(iguais(sensores, sinalj, sensores_size)){
+      Serial.println("j");
+    }
+    else if(iguais(sensores, sinalk, sensores_size)){
+      Serial.println("k");
+    }
+    else if(iguais(sensores, sinall, sensores_size)){
+      Serial.println("l");
+    }
+    else if(iguais(sensores, sinalm, sensores_size)){
+      Serial.println("m");
+    }
+    else if(iguais(sensores, sinaln, sensores_size)){
+      Serial.println("n");
+    }
+    else if(iguais(sensores, sinalo, sensores_size)){
+      Serial.println("o");
+    }
+    else if(iguais(sensores, sinalp, sensores_size)){
+      Serial.println("p");
+    }
+    else if(iguais(sensores, sinalq, sensores_size)){
+      Serial.println("q");
+    }
+    else if(iguais(sensores, sinalr, sensores_size)){
+      Serial.println("r");
+    }
+    else if(iguais(sensores, sinals, sensores_size)){
+      Serial.println("s");
+    }
+    else if(iguais(sensores, sinalt, sensores_size)){
+      Serial.println("t");
+    }
+    else if(iguais(sensores, sinalu, sensores_size)){
+      Serial.println("u");
+    }
+    else if(iguais(sensores, sinalv, sensores_size)){
+      Serial.println("v");
+    }
+    else if(iguais(sensores, sinalw, sensores_size)){
+      Serial.println("w");
+    }
+    else if(iguais(sensores, sinalx, sensores_size)){
+      Serial.println("x");
+    }
+    else if(iguais(sensores, sinaly, sensores_size)){
+      Serial.println("y");
+    }
+    else if(iguais(sensores, sinalz, sensores_size)){
+      Serial.println("z");
+    }
+    else if(iguais(sensores, sinalespaco, sensores_size)){
+      Serial.println("espaço");
+    }
+    else {
+      Serial.println("..");
+    }
+  }
+  else { /* é um intermediário e não um sinal */
+    Serial.println("...");
+  }
+
 }
